b88df9bb5e60dd26925d8fd5c9eedaa90f994148
H.M Fazeel Imam <133914414+Fazeelimam@users.noreply.github.com>
2025-06-21 18:10:53 +0500

    Delete BackEnd/api/router directory

--- BackEnd/api/router/message.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {sendMessages} from "../controller/message.js"
-
-const router = express.Router();
-
-router.post("/send", sendMessages);
-
-export default router;
