b732e35d11f788c8a1e7e111128ae458959fec61
unknown <fazeelimam6@gmail.com>
2026-02-27 01:36:22 +0500

    chages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -81,17 +81,7 @@ export default function App() {
       <FooterSection />
 
       {/* Toast notifications */}
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
+      <ToastContainer />
     </BrowserRouter>
   );
 }
